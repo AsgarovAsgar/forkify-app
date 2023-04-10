5cdafa5430be8e23c74fd5083bdbf90c1169bc40
Asgarov Asgar <asgara@maestro.st>
2023-04-11 00:06:52 +0400

    implemented search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,14 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js'
+import resultsView from './views/resultsView.js'
 
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept()
+}
 
 const controlRecipes = async function() {
   try {
@@ -29,11 +30,15 @@ const controlRecipes = async function() {
 
 const controlSearchResults = async function() { // this is subscriber function
   try {
+    // 1) Get search query
     const query = searchView.getQuery()
     if(!query) return
 
+    // 2) Load search results
     await model.loadSearchResults(query)
-    console.log('state.search', model.state.search);
+
+    // 3) Render results
+    resultsView.render(model.state.search.results)
 
   } catch (err) {
     console.log(err);
