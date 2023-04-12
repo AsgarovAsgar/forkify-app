91c65cca23ff080e476854589b3e4ff90f5d8926
Asgarov Asgar <asgara@maestro.st>
2023-04-13 01:04:18 +0400

    completed pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js'
 import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js'
 import resultsView from './views/resultsView.js'
+import paginationView from './views/paginationView.js'
 
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
@@ -39,16 +40,28 @@ const controlSearchResults = async function() { // this is subscriber function
 
     // 3) Render results
     // resultsView.render(model.state.search.results)
-    resultsView.render(model.getSearchResultsPage(1))
+    resultsView.render(model.getSearchResultsPage())
+
+    // 4) render initial pagination buttons
+    paginationView.render(model.state.search)
 
   } catch (err) {
     console.log(err);
   }
 }
 
+const controlPagination = function(goToPage) { // this is subscriber function
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage))
+
+  // 2) render NEW pagination buttons
+  paginationView.render(model.state.search)
+}
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes)
   searchView.addHandlerSearch(controlSearchResults)
+  paginationView.addHandlerClick(controlPagination)
 }
 
 init()
